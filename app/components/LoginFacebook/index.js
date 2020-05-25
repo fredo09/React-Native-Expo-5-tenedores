a7900be7a2ca3b5ce5fe51aa8332c800a75e9c07
Fredy <desaMts1806@MacBook-Pro-de-macpro-4.local>
2020-05-25 01:08:44 -0500

    Agregando funcionalidad al boton de facebook

--- app/components/LoginFacebook/index.js
+++ app/components/LoginFacebook/index.js
@@ -4,9 +4,9 @@
 
 import React, { useState }from 'react';
 import { SocialIcon } from 'react-native-elements';
-// import { FacebookApi } from './../../utils/Social';
+import { FacebookApi } from './../../utils/Social' ;
 import { Loading } from './../Loading';
-// import * as Facebook from 'expo-facebook';
+import * as Facebook from 'expo-facebook';
 import * as Firebase from 'firebase';
 
 export const SocialButtonFacebook = (props) => {
@@ -14,29 +14,35 @@ export const SocialButtonFacebook = (props) => {
     const [ isLoading, setLoading ] = useState(false);
 
     const login_facebook = async () => {
-        /*setLoading(true);
+        console.log('Login con Facebook');
 
-        const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-            FacebookApi.application_id,
-            { permissions: FacebookApi.permissions }
-        );
-        switch(type){
+        await Facebook.initializeAsync(FacebookApi.application_id);
+
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync({ 
+            FacebookApi
+        });
+    
+        switch (type) {
             case 'success':
-                const credentials = Firebase.auth.FacebookAuthProvider(token);
+                const credentials = Firebase.auth.FacebookAuthProvider.credential(token);
                 await Firebase
                     .auth()
                     .signInWithCredential(credentials)
                     .then(() => {
                         navigation.navigate('Account');
                     })
-                    .catch(() => {
-                        toastRef.current.show('Ocurrio un Error X(');
-                    })
+                    .catch((err) => {
+                        console.log(err)
+                        toastRef.current.show('Ocurrio un Error');
+                    });
+                break;
             case 'cancel':
-                toastRef.current.show('No se pudo hacer login con Facebook');
+                toastRef.current.show('No se pude hacer login con Facebook');
+                break;
+            default:
+                toastRef.current.show('Ocurrio un error, intena mas tarde!');
+                break;
         }
-        setLoading(false);*/
-        console.log('Login con Facebook');
     }
 
     return(
---
+++ app/utils/Social.js
@@ -0,0 +1,4 @@
+export const FacebookApi = {
+    application_id : '272963717189055',
+    permissions: ['profile_profile', 'email']
+  }
