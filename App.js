6f74d39c3709e9b9309532f8ea56abbc65590bd8
Fredy <desaMts1806@MacBook-Pro-de-macpro-4.local>
2020-05-30 19:53:39 -0500

    Agregando Modificaciones para persmisos y oculatando el warning de setting a timer terminando el panel de usaurio.

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import { NavigationStacks }  from './app/navigation/navigation';
 import { firebaseConfigApp } from './app/utils/Firebase';
+
+YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
   return (
